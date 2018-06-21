9f6e722f1d3784b56a74fc53c55268ef3da2d560
Harshit Raj <raj.0489@gmail.com>
2018-06-21 11:05:41 -0400

    initial commit

---
+++ CSVAnalyzer/src/main/java/com/raj/validity/CSVAnalyzer/CsvAnalyzerApplication.java
@@ -0,0 +1,12 @@
+package com.raj.validity.CSVAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvAnalyzerApplication.class, args);
+	}
+}
